04fdeb5f1f4fe4973ce62c6844b1e967c36ee35e
Arthur Villar <=>
2020-10-07 00:10:51 -0400

    Finish menu layout

--- resources/views/welcome/sections/contact.blade.php
+++ resources/views/welcome/sections/contact.blade.php
@@ -3,7 +3,7 @@
 		<div class="col-lg-7 col-md-8 col-10 mx-auto text-center">
 			<h3 class="text-center mb-3"><strong>WE'D LOVE TO HEAR FROM YOU</strong></h3>
 			<p>Whether you have a question about our services, testing, products, or anything else, our team is ready to answer all your questions</p>
-			<a href="mailto:contact@improve-ks.com" class="btn btn-outline-primary btn-lg border-1x">@fa(['icon' => 'envelope'])GET IN TOUCH</a>
+			<a href="{{route('home')}}" class="btn btn-outline-primary btn-lg border-1x">@fa(['icon' => 'envelope'])GET IN TOUCH</a>
 		</div>
 	</div>
 </section>
