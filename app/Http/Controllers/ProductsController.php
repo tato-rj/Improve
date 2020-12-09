c00a510a965ce30f848743820c99526af84de2a7
Arthur Villar <=>
2020-12-09 15:24:04 -0500

    Add option to filter produts

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -16,7 +16,7 @@ class ProductsController extends Controller
     public function index()
     {
         $products = Product::retrieve();
-
+        
         return view('products.index', compact('products'));
     }
 
