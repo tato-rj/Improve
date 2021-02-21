401684bd1683aee665012a7d3864d68d523b3e70
Arthur Villar <=>
2021-02-21 15:28:19 -0500

    Add href to main page button

--- resources/views/welcome/sections/_lead.blade.php
+++ resources/views/welcome/sections/_lead.blade.php
@@ -3,7 +3,7 @@
 		<div class="col-6 col-10 mx-auto text-white text-center">
 			<h1 class="font-primary"><strong>@lang('Don\'t stay the same – IMPROVE')</strong></h1>
 			<p class="text-uppercase lead mb-5" style="font-size: 1.18em; letter-spacing: .2em">@lang('performance testing, program development & monitoring')</p>
-			<a href="" class="btn btn-outline-white btn-lg font-primary border-1x text-uppercase">@lang('Unlock your potential')</a>
+			<a href="mailto:{{email()}}" class="btn btn-outline-white btn-lg font-primary border-1x text-uppercase">@lang('Unlock your potential')</a>
 		</div>
 	</div>
 </section>
