4eb8b35df3e03e8590b799919d8215a4878341a0
Arthur Villar <=>
2021-02-21 15:37:21 -0500

    Add href to main page button

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -77,7 +77,7 @@ class Product extends Model
 	{
 		return $query->filter(request()->view, ['onsale' => 'discount'])
 					 ->sort(request()->sortby)
-					 ->paginate(6);
+					 ->paginate(20);
 	}
 
 	public function scopeFilter($query, $input, $attr)
