e4869488c1d5f517e1d9343fe6f43908163f3b2c
Arthur Villar <=>
2021-02-21 15:58:13 -0500

    Add href to main page button

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -1,6 +1,6 @@
 <iframe 
 	class="w-100 h-100 d-block" 
-	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.8543934809645!2d21.171784115427204!3d42.66444087916762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549f2782ac6997%3A0x254ec3536ef04959!2sIsa%20Boletini%2C%20Prishtin%C3%AB!5e0!3m2!1sen!2sus!4v1609276283670!5m2!1sen!2sus" 
+	src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2934.2557061962193!2d21.1577468!3d42.6559364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549eef185412cb%3A0x3c4ff6259ec3b2ec!2sIlaz%20Kodra%2C%20Prishtin%C3%AB!5e0!3m2!1sen!2sus!4v1613941028825!5m2!1sen!2sus" 
 	frameborder="0" 
 	style="border:0; min-height: 320px" 
 	allowfullscreen="" 
